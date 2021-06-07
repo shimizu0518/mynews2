23325a429dd0c62c5c19d699ef7305f0dca80db9
shimizu0518 <bettybettybetty518@gmail.com>
2021-06-07 15:56:28 +0000

    課題１６

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -30,13 +30,19 @@ class ProfileController extends Controller
     return redirect('admin/profile/create');
   }
 
-  public function edit()
+  public function edit(Request $request)
   {
-    return view('admin.profile.edit');
+    // News Modelからデータを取得する
+    $profile = Profile::find($request->id);
+    if (empty($profile)) {
+      abort(404);    
+    }
+    return view('admin.profile.edit', ['profile_form' => $profile]);
   }
 
-  public function update()
+  public function update(Request $request)
   {
+    $this->validate($request, Profile::$rules);
     return redirect('admin/profile/edit');
   }
 }
